c180af207b93ac500421b07be6cb74735cbc5bbe
sibaprasad <sibaprasad.x.mohanty@gmail.com>
2021-06-15 12:55:46 +0100

    Removed Clean architecture code

--- ArchitectureAndDesignPattern/app/src/main/java/spm/architecture/designpattern/MainActivity.kt
+++ ArchitectureAndDesignPattern/app/src/main/java/spm/architecture/designpattern/MainActivity.kt
@@ -4,10 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import spm.architecture.designpattern.cleanArchitecture.CleanArchitectureActivity
 import spm.architecture.designpattern.mvi.MviActivity
 import spm.architecture.designpattern.mvp.MvpActivity
 import spm.architecture.designpattern.mvvm.MvvmActivity
 import spm.architecture.designpattern.mvvmWithDataBinding.MvvmDatabindingActivity
+import spm.architecture.designpattern.volleyWithMvp.VolleyMvpActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +30,14 @@ class MainActivity : AppCompatActivity() {
 
         buttonMvi.setOnClickListener {
             startActivity(Intent(this, MviActivity::class.java))
+        }
+
+        buttonVolleyWithMvp.setOnClickListener {
+            startActivity(Intent(this, VolleyMvpActivity::class.java))
+        }
+
+        buttonCleanArchitecture.setOnClickListener {
+            startActivity(Intent(this, CleanArchitectureActivity::class.java))
         }
 
     }
